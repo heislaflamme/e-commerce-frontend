e10f26d17bed869567c78aba214618c6839b9ecc
Chidubem <emekaogbuchidubem@gmail.com>
2026-03-15 06:26:17 +0100

    ...

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,8 +7,15 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg"
   }),
+  baseURL: process.env.BETTER_AUTH_URL, 
   emailAndPassword: {
     enabled: true,
+  },
+  socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
   },
   plugins: [tanstackStartCookies()],
 })
